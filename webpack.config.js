85a1f60656eb7a2a242f19b140c2e90fc0b99b37
John Talbot <ztalbot2000@gmail.com>
2022-10-15 17:18:47 -0400

    fix: Examples seem to run

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,10 @@ const webpack = require('webpack');
 
 const ESLintPlugin = require('eslint-config-prettier');
 
+let performanceOptions =
+{
+};
+
 let devServerOptions =
 {
    //stats: {
@@ -38,8 +42,6 @@ let devServerOptions =
 
    // serves everything from our dist directory in the project root:
    static: {
-      //publicPath: '/dist/',
-      //directory: path.join(__dirname, 'dist'),
       directory: path.join(__dirname, 'dist'),
       watch: true
    },
@@ -103,6 +105,14 @@ let devServerOptions =
       console.log( 'Listening on port:', port );
    },
 
+   devMiddleware: {
+      index: true,
+      mimeTypes: { phtml: 'text/html' },
+      publicPath: 'dist',
+      serverSideRender: true,
+      writeToDisk: true,
+    },
+
    // Tells dev-server to open the browser after server had been started.
    //open: { },
 
@@ -114,157 +124,167 @@ let devServerOptions =
    //       However I confused it with devServer.index which
    //       cannot be set via cli. Why the two is beyond me?
    //openPage: 'index.html'
-   open:
+   open: [ "index.html" ]
+};
+let webpackOptions =
+{
+   // Since this is processed as a command line option (because of argv...)
+   // the mode must be set so that it can be returned as part of the big
+   // config entry.
+   mode: "production",
+
+   performance: performanceOptions,
+
+   entry: './src/index.ts',
+   output: {
+      // Default in webpack V5
+      //path: path.resolve(__dirname, 'dist'),
+      filename: 'pixi_ui.js',
+      // The library name means you would access it via pixi-ui.button.
+      library: 'pixi_ui',
+
+      // Add vitual path for generating the bundle files
+      publicPath: 'dist',
+
+      // Turn off pathInfo, increasing build time
+      pathinfo: false,
+   },
+
+
+   devServer: devServerOptions,
+
+   plugins: [
+      // Too messy, deal with later
+      // new ESLintPlugin({
+      //    files: ['./app/partKart/**/*.ts',
+      //            './app/src/*.ts'
+      //           ]
+      // }),
+
+      // Add a plugins section to your webpack.config.js to let
+      // know Webpack that the global PIXI variable make reference
+      // to pixi.js module. For instance:
+      new webpack.ProvidePlugin({
+         PIXI: 'pixi.js'
+      })
+   ],
+
+   //Default
+   target: 'web',
+   externals: {"pixi.js": "PIXI",
+               "fs": "require('fs')" },
+   resolve: {
+      // Add '.ts' as resolvable extensions.
+      extensions: [".ts", ".js"],
+      alias: {
+         $: "jquery/src/jquery",
+      }
+   },
+   optimization: {
+      minimize: false,
+   },
+
+   // Enable sourcemaps for debugging webpack's output.
+   // // devtool: "source-map" // without any, there is no sourcemap
+   //devtool: "eval"  // none
+   devtool: "source-map", // one big blob
+   //devtool: "eval-source-map"  // none
+
+   module:
    {
-      target: [ 'index.html' ]
+      rules: [
+         {
+            test: /\.js$/,
+            include: [path.resolve(__dirname, "src")],
+            exclude: [
+               // This would be a regular expression
+               /node_modules/,
+               // This would be a path
+               // Omit stuff to be worked on
+               '/test/somepath/'      // Omit all test/somepath
+            ],
+            use: {
+               loader: 'babel-loader',
+               // Note. Do not put other compile options here !!!
+               // ts-loader or babel-loader may override them.
+               // i.e ts-loader listFiles: true or
+               //     ts-loader outdir: 'js'
+            }
+         },
+         {
+            test: /\.ts$/,
+            include: [path.resolve(__dirname, "src"),
+                      path.resolve(__dirname, "test/src")],
+            exclude: [
+               // This would be a regular expression
+               /node_modules/
+               // This would be a path
+               // Omit stuff to be worked on
+            ],
+            use: {
+               loader: "ts-loader",
+               // Note. Do not other put compile options here !!!
+               // ts-loader or babel-loader may override them.
+               // i.e ts-loader listFiles: true or
+               //     ts-loader outdir: 'js'
+               options: {
+                  transpileOnly: true,
+                  experimentalWatchApi: true,
+               },
+            }
+         }
+      ]
    }
 };
 
 module.exports = (env, argv) =>
 {
    console.log("Not bundling pixi.js");
-   Externals= {"pixi.js": "PIXI",
-               "fs": "require('fs')" };
 
-   watchOptions = {
+   webpackOptions.watchOptions = {
       // Add a delay before rebuilding once the first file changed
       aggregateTimeout:  600,
       poll: 5000
    };
 
    // The default 'mode' to use.
-   Mode='production';
+   webpackOptions.mode='production';
 
    if ( argv )
    {
-      const r = Object.keys(argv);
-      console.log("argv = " + r);
-      if ( argv.mode )
-         if (argv.mode === 'development' )
-            // Since this is processed as a command line option
-            // (because of argv...) the mode must be set so that it
-            // can be returned as part of the big config entry.
-            Mode='development';
-         else if ( argv.mode !== 'production' )
-            throw new error('unhandled mode:' + argv.mode);
+      //const r = Object.keys(argv);
+      //console.log("argv = " + r);
+      //console.log("argv.mode = " + argv.mode );
 
-      console.log("argv = " + argv);
-      console.log("argv['openTarget'] = " + argv['openTarget']);
-      if ( argv['openTarget'] )
+      if ( argv.mode === undefined || argv.mode === 'development' )
       {
-         console.log("In if argv = " + argv[ 'openTarget' ]);
-         console.log("devServerOptions.open = " + devServerOptions.open );
-         devServerOptions.open = { target: argv[ 'openTarget' ] };
-         //devServerOptions.open.target = [ ];
-         //devServerOptions.open.target.push( argv['openTarget'] );
-         console.log("telling devServer to open:" + devServerOptions.open);
-         console.log("telling devServer to open.target:" + devServerOptions.open.target);
+         // Since this is processed as a command line option
+         // (because of argv...) the mode must be set so that it
+         // can be returned as part of the big config entry.
+         webpackOptions.mode='development';
+         webpackOptions.performance = {
+            hints: false,
+            maxEntrypointSize: 512000,
+            maxAssetSize: 512000
+         };
       }
+      else if ( argv.mode === 'production' )
+      {
+         // For now
+         // Will minify later ...
+         webpackOptions.performance = {
+            hints: false,
+            maxEntrypointSize: 512000,
+            maxAssetSize: 512000
+         };
+
+      } else
+         throw new Error('unhandled mode:' + argv.mode);
+
    }
 
-   return {
-      // Since this is processed as a command line option (because of argv...)
-      // the mode must be set so that it can be returned as part of the big
-      // config entry.
-      mode: Mode,
+   //const util = require('util')
+   //console.log(util.inspect(webpackOptions, {showHidden: false, depth: null, colors: true}));
 
-      entry: './src/index.ts',
-      output: {
-         path: path.resolve(__dirname, 'dist'),
-         filename: 'pixi_ui.js',
-         // The library name means you would access it via pixi-ui.button.
-         library: 'pixi_ui',
-
-         // Add vitual path for generating the bundle files
-         publicPath: 'dist',
-
-         // Turn off pathInfo, increasing build time
-         pathinfo: false,
-      },
-
-
-      devServer: devServerOptions,
-
-      plugins: [
-         // Too messy, deal with later
-         // new ESLintPlugin({
-         //    files: ['./app/partKart/**/*.ts',
-         //            './app/src/*.ts'
-         //           ]
-         // }),
-
-         // Add a plugins section to your webpack.config.js to let
-         // know Webpack that the global PIXI variable make reference
-         // to pixi.js module. For instance:
-         new webpack.ProvidePlugin({
-            PIXI: 'pixi.js'
-         })
-      ],
-
-      //target: 'umd',
-      //target: 'web',
-      externals: Externals,
-      resolve: {
-         // Add '.ts' as resolvable extensions.
-         extensions: [".ts", ".js"],
-         alias: {
-            $: "jquery/src/jquery",
-         }
-      },
-      optimization: {
-         minimize: false,
-      },
-
-      // Enable sourcemaps for debugging webpack's output.
-      // // devtool: "source-map" // without any, there is no sourcemap
-      //devtool: "eval"  // none
-      devtool: "source-map", // one big blob
-      //devtool: "eval-source-map"  // none
-
-      module:
-      {
-         rules: [
-            {
-               test: /\.js$/,
-               include: [path.resolve(__dirname, "src")],
-               exclude: [
-                  // This would be a regular expression
-                  /node_modules/,
-                  // This would be a path
-                  // Omit stuff to be worked on
-                  '/test/somepath/'      // Omit all test/somepath
-               ],
-               use: {
-                  loader: 'babel-loader',
-                  // Note. Do not put other compile options here !!!
-                  // ts-loader or babel-loader may override them.
-                  // i.e ts-loader listFiles: true or
-                  //     ts-loader outdir: 'js'
-               }
-            },
-            {
-               test: /\.ts$/,
-               include: [path.resolve(__dirname, "src"),
-                         path.resolve(__dirname, "test/src")],
-               exclude: [
-                  // This would be a regular expression
-                  /node_modules/
-                  // This would be a path
-                  // Omit stuff to be worked on
-               ],
-               use: {
-                  loader: "ts-loader",
-                  // Note. Do not other put compile options here !!!
-                  // ts-loader or babel-loader may override them.
-                  // i.e ts-loader listFiles: true or
-                  //     ts-loader outdir: 'js'
-                  options: {
-                     transpileOnly: true,
-                     experimentalWatchApi: true,
-                  },
-               }
-            }
-         ]
-      }
-   };
+   return webpackOptions;
 };
